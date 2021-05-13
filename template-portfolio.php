1b74b7f9ff9ddd173a014ad66afc961c995dec60
Cory <caraket@gmail.com>
2021-05-13 10:17:18 -0500

    Added the portfolio template

---
+++ template-portfolio.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+    Template Name: Portfolio
+*/
+?>
+
+<?php get_header(); ?>
+
+<section class="page-wrap">
+    <div class="wrapper">
+        <?php if(has_post_thumbnail()):?>
+            <img class="featured-image" src="<?php the_post_thumbnail_url('featured-large'); ?>" alt="<?php the_title(); ?>" class="img-fluid img-thumbnail">
+        <?php endif;?>
+        
+
+        <div class="content">
+            <?php get_template_part('includes/section', 'content'); ?>
+        </div>
+
+
+    </div>
+</section>
+
+
+
+<?php get_footer(); ?>
